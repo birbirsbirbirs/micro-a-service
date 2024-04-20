58c05719aafd65b7ed978913708c5bd6a79ac197
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-19 22:23:19 -0500

    webclient updated

--- src/main/java/co/pitam/aservice/controller/HeroController.java
+++ src/main/java/co/pitam/aservice/controller/HeroController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 
@@ -22,6 +23,7 @@ public class HeroController {
     private String bServiceUrl;
 
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
     private final Tracer tracer;
     private final PtmAsynService ptmAsynService;
 
@@ -36,6 +38,23 @@ public class HeroController {
 
         Hero hero = restTemplate.getForObject(bServiceUrl, Hero.class);
         log.info("Hero from b-service: {}", hero);
+        return hero;
+    }
+
+    @GetMapping("/webclient")
+    public Hero getHeroWebClient() {
+        String randomString = UUID.randomUUID().toString();
+        log.info("updating power value webclient: {}", randomString);
+        tracer.createBaggageInScope("power", "power-" + randomString);
+
+        ptmAsynService.runLog();
+
+        Hero hero = webClient.get().uri(bServiceUrl)
+                .retrieve()
+                .bodyToMono(Hero.class)
+                .block();
+
+        log.info("Hero from webclient, b-service: {}", hero);
         return hero;
     }
 }
