e2bd47fb7a3f4eda5b08cfc2b5bbae6f9e119913
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-20 15:10:54 -0500

    rabbit mq configuration

--- src/main/java/co/pitam/aservice/service/PublishHero.java
+++ src/main/java/co/pitam/aservice/service/PublishHero.java
@@ -2,31 +2,35 @@ package co.pitam.aservice.service;
 
 
 import co.pitam.aservice.model.Hero;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.function.Supplier;
+import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PublishHero {
+    private final StreamBridge streamBridge;
 
+    private final Tracer tracer;
 
-    private final BlockingQueue<Hero> orderEvent = new LinkedBlockingQueue<>();
-
-    @Bean
-    public Supplier<Hero> outputsns() {
-        return () -> this.orderEvent.poll();
-    }
+    private final ObservationRegistry observationRegistry;
 
 
     public void sendOrder(Hero hero) {
-        this.orderEvent.offer(hero);
+        String randomString = UUID.randomUUID().toString();
+        log.info("updating power producer binder: {}", randomString);
+        tracer.createBaggageInScope("power", "power-" + randomString);
+        Observation.createNotStarted("stream.producer", observationRegistry).observe(() -> {
+            log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer", this.tracer.currentSpan().context().traceId());
+            this.streamBridge.send("output-out-0", hero);
+        });
         log.info("Event sent: " + hero);
     }
 
