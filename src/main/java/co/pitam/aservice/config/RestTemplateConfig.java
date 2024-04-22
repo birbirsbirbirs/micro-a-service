e4ec86b0eecd621200a01d81e999afbdf5d64865
birbirs\birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-04-21 21:21:54 -0500

    opentelemetry-spring-boot-starter

--- src/main/java/co/pitam/aservice/config/RestTemplateConfig.java
+++ src/main/java/co/pitam/aservice/config/RestTemplateConfig.java
@@ -1,8 +1,5 @@
 package co.pitam.aservice.config;
 
-import brave.Tracing;
-import brave.http.HttpTracing;
-import brave.spring.web.TracingClientHttpRequestInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +7,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+
     @Bean
-    public HttpTracing create(Tracing tracing) {
-        return HttpTracing
-                .newBuilder(tracing)
-                .build();
-    }
-    @Bean
-    public RestTemplate restTemplate(HttpTracing httpTracing) {
+    public RestTemplate restTemplate() {
         return new RestTemplateBuilder()
-                .interceptors(TracingClientHttpRequestInterceptor.create(httpTracing))
                 .build();
     }
 }
